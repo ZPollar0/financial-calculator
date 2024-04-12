45a5f2ea7fd77f83a6cc6c1123410fa67bd41f17
ZPollar0 <zpollard2000@gmail.com>
2024-04-12 14:27:45 -0400

    completed workshop assignment: Financial Calculators

--- src/main/java/com/pluralsight/Calculator2.java
+++ src/main/java/com/pluralsight/Calculator2.java
@@ -1,5 +1,25 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 
 public class Calculator2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How much are you depositing?");
+        double deposit = input.nextDouble();
+        System.out.println("Please enter the interest rate.");
+        double InterestRate = input.nextDouble();
+        System.out.println("How many years will you compound?");
+        int years = input.nextInt();
+
+        double compound = deposit * (Math.pow(InterestRate , years));
+        System.out.println("Your compound after " + years + " years is: " + compound);
+
+        double interestAmount = compound - deposit;
+        System.out.println("You built up " + interestAmount + " in interest over " + years + " years." );
+
+
+
+
+    }
 }
